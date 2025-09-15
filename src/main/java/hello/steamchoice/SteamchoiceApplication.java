1c62b7846b557b05f9d938684407ebed08fde1f4
csy10001 <fuitshine@gmail.com>
2025-09-15 16:03:28 +0900

    first commit

---
+++ src/main/java/hello/steamchoice/SteamchoiceApplication.java
@@ -0,0 +1,13 @@
+package hello.steamchoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamchoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamchoiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/steamchoice/SteamchoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.steamchoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamchoiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
